9ebcae5c6438f725ad41da635d03f8ec4a7e3266
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-27 21:58:28 +0300

    wtf

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
+    
 <?php
-include('vendor/autoload.php');
-use Telegram\Bot\Api;
-$telegram = new Api('713953239:AAFiRmir3z-JsMnDMmGdQ4twvV2nzLpADGs');
-$result = $telegram -> getWebhookUpdates();
-$text = $result["message"]["text"];
-$chat_id = $result["message"]["chat"]["id"];
-$name = $result["message"]["from"]["username"];
-if($text) {
-  if ($text == "/start") {
-    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' =>'Добро пожаловать, ' . $name . '!' ]);
-  } else{
-    	 $reply = "Добро пожаловать, ".$name."!";
+  include('vendor/autoload.php'); 
+  use Telegram\Bot\Api; 
+  $telegram = new Api('831061547:AAFwm0s2dLQIWLhRHJljKVVRv4aTzwpbgI0'); 
+  $result = $telegram -> getWebhookUpdates(); 
+  $text = $result["message"]["text"];
+  $chat_id = $result["message"]["chat"]["id"];  
+  $name = $result["message"]["from"]["username"];
+  if($text) {
+    if ($text == "/start" and $name) {
+      $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' =>'Добро пожаловать, ' . $name . '!' ]);
+    } elseif($text == "/start" and !$name) {
+      $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' =>'Добро пожаловать, незнакомец!' ]);
+    } elseif ($text == "/sayHello") {
+      $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Hello World" ]);
+    } else {
+     	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
     }
   }
 ?>
