d1c14edfb49243abb6f80a46cb273a1fcccd3595
artyRay12 <artysha.rylit12@gmail.com>
2019-06-27 18:35:24 +0300

    Idk what im doing

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include('vendor/autoload.php');
+use Telegram\Bot\Api;
+$telegram = new Api('713953239:AAFiRmir3z-JsMnDMmGdQ4twvV2nzLpADGs');
+$result = $telegram -> getWebhookUpdates();
+$text = $result["message"]["text"];
+$chat_id = $result["message"]["chat"]["id"];
+$name = $result["message"]["from"]["username"];
+if($text) {
+    if ($text == "/start") {
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Добро пожаловать!" ]);
+    } else{
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
+    }
+}
+?>
