33a46a4379c8450a4b7d854af59a8f13a02399a7
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-27 16:40:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-  echo 'Привет'
+  include('vendor/autoload.php'); 
+  use Telegram\Bot\Api; 
+  $telegram = new Api('831061547:AAFwm0s2dLQIWLhRHJljKVVRv4aTzwpbgI0'); 
+  $result = $telegram -> getWebhookUpdates(); 
+  $text = $result["message"]["text"];
+  $chat_id = $result["message"]["chat"]["id"];  
+  $name = $result["message"]["from"]["username"];
+  if($text) {
+  if ($text == "/start") {
+    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Добро пожаловать!" ]);
+  } else{
+    	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
+    }
+  }
 ?>
