fc8b18ba3e240b1dfb708d144c6298392ed5d372
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-27 20:57:13 +0300

    debug

--- index.php
+++ index.php
@@ -7,15 +7,10 @@ $text = $result["message"]["text"];
 $chat_id = $result["message"]["chat"]["id"];
 $name = $result["message"]["from"]["username"];
 if($text) {
-    if ($text == "/sayhello") {
-        if (srtlen($name) != 0)
-            $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Добро пожаловать!, " . $name . "!"]);
-        else
-            $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Добро пожаловать незнакомец"]);
-    else
-    {
-    $reply = "Введите что-то другое"
+  if ($text == "/start") {
+    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Добро пожаловать!" ]);
+  } else{
+    	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
     }
-}
+  }
 ?>
-
