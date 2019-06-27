e284de878cbcc3911b57b6ec67189cb69cf3fbbf
artyRay12 <artysha.rylit12@gmail.com>
2019-06-27 19:58:15 +0300

    added new lib longman

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $text = $result["message"]["text"];
 $chat_id = $result["message"]["chat"]["id"];
 $name = $result["message"]["from"]["username"];
 if($text) {
-    if ($text == "/start") {
+    if ($text == "/sayhello") {
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Добро пожаловать!" ]);
     } else{
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
