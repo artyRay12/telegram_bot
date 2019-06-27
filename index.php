04db882b5f9b659217511463d1f75acf5f81ac83
artyRay12 <artysha.rylit12@gmail.com>
2019-06-28 00:14:48 +0300

    add fixer API..i hope!!

--- index.php
+++ index.php
@@ -6,12 +6,16 @@ $result = $telegram -> getWebhookUpdates();
 $text = $result["message"]["text"];
 $chat_id = $result["message"]["chat"]["id"];
 $name = $result["message"]["from"]["username"];
+$keyboard = [["Начать викторину"]]; //Клавиша которая нихера не появляется
 if($text) {
-    if ($text == "/sayhello") {
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Добро пожаловать!" ]);
-    } else{
+    if ($text == "/start" and $name) {
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' =>'Добро пожаловать, ' . $name . '!' ]);
+    } elseif($text == "/start" and !$name) {
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' =>'Добро пожаловать, незнакомец!' ]);
+    } elseif ($text == "/sayHello") {
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Hello World" ]);
+    } else {
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
     }
 }
 ?>
-
