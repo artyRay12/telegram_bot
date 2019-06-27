9f4ca0a3bdbe7d5f3f14d7b7efff8a6660cdf00f
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-27 20:17:06 +0300

    button test
    meow

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $result = $telegram -> getWebhookUpdates();
 $text = $result["message"]["text"];
 $chat_id = $result["message"]["chat"]["id"];
 $name = $result["message"]["from"]["username"];
+$inline_button1 = array("text"=>"Google url","url"=>"http://google.com");
 if($text) {
     if ($text == "/sayhello") {
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Добро пожаловать!" ]);
