fa67f02f1f7496d099a0d3b8b349bbcdc88dc15d
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-28 14:52:56 +0300

    keyboeard test

--- index.php
+++ index.php
@@ -4,12 +4,12 @@ use Telegram\Bot\Api;
 use Fadion\Fixerio\Exchange;
 use Fadion\Fixerio\Currency;
 
-$exchange = new Exchange();
+/*$exchange = new Exchange();
 $exchange->key('f22838f03ab3c8f3ff5f7e119f870dfe');
 $exchange->base(Currency::USD);
 $exchange->symbols(Currency::EUR, Currency::GBP);
+$rates = $exchange->get();*/
 
-$rates = $exchange->get();
 $telegram = new Api('713953239:AAFiRmir3z-JsMnDMmGdQ4twvV2nzLpADGs');
 $result = $telegram -> getWebhookUpdates();
 $text = $result["message"]["text"];
@@ -17,9 +17,32 @@ $chat_id = $result["message"]["chat"]["id"];
 $name = $result["message"]["from"]["username"];
 $keyboard = [["Начать викторину"]]; //Клавиша которая нихера не появляется
 
-$rates = (new Exchange())->key("f22838f03ab3c8f3ff5f7e119f870dfe")->symbols(Currency::USD, Currency::GBP)->get();
+/*$rates = (new Exchange())->key("f22838f03ab3c8f3ff5f7e119f870dfe")->symbols(Currency::USD, Currency::GBP)->get();
 print $rates['EUR'];
-print $rates[Currency::GBP];
+print $rates[Currency::GBP];*/
+
+//----====KEYBOARD TEST====-----
+$keyboard = [
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3'],
+         ['0']
+];
+
+$reply_markup = $telegram->replyKeyboardMarkup([
+	'keyboard' => $keyboard, 
+	'resize_keyboard' => true, 
+	'one_time_keyboard' => true
+]);
+
+$response = $telegram->sendMessage([
+	'chat_id' => 'CHAT_ID', 
+	'text' => 'Hello World', 
+	'reply_markup' => $reply_markup
+]);
+
+$messageId = $response->getMessageId();
+//-=-=-=-=-=-=-=-=--=-=-=-=
 
 
 if($text) {
