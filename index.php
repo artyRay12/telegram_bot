e7e8903933e6fb366dca643788cdfb7f6e9720ce
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-29 22:15:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -23,8 +23,8 @@ print $rates['EUR'];
 print $rates[Currency::GBP];*/
 
 //----====KEYBOARD TEST====-----
-$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+//$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+//$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
 
 //-=-=-=-=-=-=-=-=--=-=-=-=
 if($text) {
