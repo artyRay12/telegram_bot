14e18cab3353c221c31473573a640639294998a2
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-30 23:15:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ include('vendor/autoload.php');
 use Telegram\Bot\Api;
 use Fadion\Fixerio\Exchange;
 use Fadion\Fixerio\Currency;
-
 $telegram = new Api('713953239:AAFiRmir3z-JsMnDMmGdQ4twvV2nzLpADGs');
 $result = $telegram -> getWebhookUpdates();
 $text = $result["message"]["text"]; //Текст сообщения
@@ -15,7 +14,6 @@ $question = "Если ты был супом, то каким супом ты б
 $questionNumber = 1;
 $posAnswer0 = "Борщ с перчиком";
 $posAnswer1 = "Щи с чесночком";
-
 function pringMsg($msg) {
     return "Game is started!!!!!!";
 }
@@ -37,48 +35,39 @@ if ($text) {
     }
 }
 $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
-
-
 function getQuestById($questionNumber, $question) {
     if ($questionNumber == 1) {
         return "На что похож твой код?";
     }
-
     if ($questionNumber == 2) {
         return "Как вы относитесь к Иисусу?";
     }
 }
-
 function getPosAnswersById($questionNumber){
     if ($questionNumber == 1) {
         return array("На дерьмо", "На гавно");
     }
-
     if ($questionNumber == 1) {
         return array("Отличный мужик", "Жаль что распяли");
     }
 }
-
 function answerAnalisys($questionNumber) {
     if ($questionNumber == 1 AND (($text == $posAnswer0) OR ($text = $posAnswer0)))
         echo "<br/>You fucking damn right";
 }
-
 while ($start == TRUE) {
-    $question = getQuestById($questionNumber, $question);
-    list($posAnswer0, $posAnswer1) = getPosAnswersById($questionNumber);
-    
+    $question = getQuestById($questionNumber, $question); //Меняю вопрос
+    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $question, 'reply_markup' => $reply_markup]);  //печатаю вопрос
+
+    list($posAnswer0, $posAnswer1) = getPosAnswersById($questionNumber);// меняю кнопки
+    $keyboard = [[$posAnswer0], [$posAnswer1]];
     $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard,
-            'resize_keyboard' => true,
-            'one_time_keyboard' => true]);
-    
-    answerAnalisys($questionNumber);
+        'resize_keyboard' => true,
+        'one_time_keyboard' => true]);
+
+    answerAnalisys($questionNumber); // анализ ответа
     $questionNumber = $questionNumber + 1;
     if ($questionNumber == 2)
-      $start = FALSE;
+        $start = FALSE;
 }
-
-
-
-
 ?>
