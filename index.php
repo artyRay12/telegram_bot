9f7a04dd95b3dcf82f5171abcec17eac77cfadcc
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-30 18:19:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,6 @@ $text = $result["message"]["text"]; //Текст сообщения
 $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
 $keyboard = [["Start"]]; //Клавиатура
-$start = FALSE;
 
 
 if ($text) {
@@ -32,11 +31,10 @@ if ($text) {
             $reply = 'Hello, stranger!';
         }
     } elseif ($text == "Start") {
-        $reply = "Game is started!!!";
+        $reply = "Game is started!!";
         $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
         $reply = "Чем отличается светло-розовый цвет, от нересто-лососевого";
         $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
-        $start = TRUE;
     }
 }
 
@@ -52,8 +50,6 @@ if ($text) {
     }
     $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
 }*/
-
-
 
 /*$rates = (new Exchange())->key("f22838f03ab3c8f3ff5f7e119f870dfe")->symbols(Currency::USD, Currency::GBP)->get();
 print $rates['EUR'];
