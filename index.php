e7291e2eed41418f7c086191a2fdae92153b1c0b
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-30 19:42:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,11 @@ include('vendor/autoload.php');
 use Telegram\Bot\Api;
 use Fadion\Fixerio\Exchange;
 use Fadion\Fixerio\Currency;
-
 /*$exchange = new Exchange();
 $exchange->key('f22838f03ab3c8f3ff5f7e119f870dfe');
 $exchange->base(Currency::USD);
 $exchange->symbols(Currency::EUR, Currency::GBP);
 $rates = $exchange->get();*/
-
 $telegram = new Api('713953239:AAFiRmir3z-JsMnDMmGdQ4twvV2nzLpADGs');
 $result = $telegram -> getWebhookUpdates();
 $text = $result["message"]["text"]; //Текст сообщения
@@ -20,7 +18,6 @@ $start = FALSE;
 function pringMsg($msg) {
     return "Game is started!!!!!!";
 }
-
 if ($text) {
     if ($text == "/start") {
         $reply = "Welcome";
@@ -33,17 +30,20 @@ if ($text) {
         } else {
             $reply = 'Hello, stranger!';
         }
-    } elseif ($text == "Start") {
+    }elseif ($text == "Start") {
         $reply = pringMsg($reply);
         $start = TRUE;
     }
 }
 $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
 if ($start == TRUE) {
+    $keyboard = [["Борщ с перчиком"], ["Щи с чесночком"]];
     $reply = "Если ты был супом, то каким супом ты бы был";
+    $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard,
+        'resize_keyboard' => true,
+        'one_time_keyboard' => true]);
 }
 $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
-
 /*if ($start) {
     $keyboard = [["Какая нахрен разница"]]; //Клавиатура
     $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard,
@@ -56,9 +56,7 @@ $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' 
     }
     $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
 }*/
-
 /*$rates = (new Exchange())->key("f22838f03ab3c8f3ff5f7e119f870dfe")->symbols(Currency::USD, Currency::GBP)->get();
 print $rates['EUR'];
 print $rates[Currency::GBP];*/
-
 ?>
