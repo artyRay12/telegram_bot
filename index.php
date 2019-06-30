4d6ca28483879ff5dcc9480a066574df5ce41c55
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-30 03:36:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,25 +15,7 @@ $result = $telegram -> getWebhookUpdates();
 $text = $result["message"]["text"]; //Текст сообщения
 $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-//$keyboard = [["Start"]]; //Клавиатура
-
-function startGame() {
-  
-  $keyboard = array(
-      array(array('callback_data'=>'/butt1','text'=>'Кнопка 1')),
-      array(array('callback_data'=>'/buut2','text'=>'Кнопка 2')),
-  );
-  $reply_markup = $telegram->replyKeyboardMarkup([ 
-      'keyboard' => $keyboard, 
-      'resize_keyboard' => true, 
-      'one_time_keyboard' => false 
-  ]);
-  $telegram->sendMessage(array(
-    'chat_id' => $chat_id,
-      'text' => 'Нажмите на одну из кнопок:',
-      'reply_markup' => $reply_markup,
-  ));
-}
+$keyboard = [["Start"]]; //Клавиатура
 
 if ($text) {    
   if ($text == "Say Hello" or $text == "/start") {
@@ -44,7 +26,20 @@ if ($text) {
     }
     //$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
     //$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-    startGame();
+     $keyboard = array(
+    array(array('callback_data'=>'/butt1','text'=>'Кнопка 1')),
+    array(array('callback_data'=>'/buut2','text'=>'Кнопка 2')),
+    );
+    $reply_markup = $telegram->replyKeyboardMarkup([ 
+        'keyboard' => $keyboard, 
+        'resize_keyboard' => true, 
+        'one_time_keyboard' => false 
+    ]);
+    $telegram->sendMessage(array(
+      'chat_id' => $chat_id,
+        'text' => 'Нажмите на одну из кнопок:',
+        'reply_markup' => $reply_markup,
+    ));
   }
 }
  
