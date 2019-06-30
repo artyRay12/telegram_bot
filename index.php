7d5e220963e0264a93ed403f254cce19005c9e3b
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-30 04:39:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,24 +17,24 @@ $chat_id = $result["message"]["chat"]["id"]; //Уникальный иденти
 $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
 $keyboard = [["Start"]]; //Клавиатура
 
-
 if($text) {
-    if ($text == "/start" and $name) {
-      $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' =>'Добро пожаловать, ' . $name . '!' ]);
-      $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard,
+    if ($text == "/start") {
+        $reply = "Welcome";
+        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard,
             'resize_keyboard' => true,
-            'one_time_keyboard' => true ]);
-      $telegram->sendMessage([ 'chat_id' => $chat_id,
+            'one_time_keyboard' => false ]);
+        $telegram->sendMessage([ 'chat_id' => $chat_id,
             'text' => $reply,
             'reply_markup' => $reply_markup ]);
-    } elseif($text == "/start" and !$name) {
-      $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' =>'Добро пожаловать!' . $result['message']['from']['first_name'] ]);
-    } elseif ($text == "/sayHello") {
-      $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Hello World" ]);
-    } else {
-     	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Запрос не является командой" ]);
+    } elseif($text == "/sayHello") {
+        if ($name) {
+            $reply = 'Hello, ' . $name . '!';
+        } else {
+            $reply = 'Hello, stranger!';
+        }
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
     }
-} 
+}
 /*
 $rates = (new Exchange())->key("f22838f03ab3c8f3ff5f7e119f870dfe")->symbols(Currency::USD, Currency::GBP)->get();
 print $rates['EUR'];
