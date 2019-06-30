43928e752ae05a839a892a52c84c6fbfee8301ca
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-30 04:47:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,24 +17,30 @@ $chat_id = $result["message"]["chat"]["id"]; //Уникальный иденти
 $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
 $keyboard = [["Start"]]; //Клавиатура
 
+function sayHello($message) {
+    $message = "You pressed button";
+    return $message;
+{
+    
 if($text) {
     if ($text == "/start") {
         $reply = "Welcome";
         $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard,
             'resize_keyboard' => true,
             'one_time_keyboard' => false ]);
-        $telegram->sendMessage([ 'chat_id' => $chat_id,
-            'text' => $reply,
-            'reply_markup' => $reply_markup ]);
-    } elseif($text == "/sayHello") {
+    } 
+    elseif($text == "/sayHello") {
         if ($name) {
             $reply = 'Hello, ' . $name . '!';
         } else {
             $reply = 'Hello, stranger!';
         }
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-    }
+    } 
+    elseif($text == "Start") {
+        sayHello($reply);
+    }  
 }
+$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
 /*
 $rates = (new Exchange())->key("f22838f03ab3c8f3ff5f7e119f870dfe")->symbols(Currency::USD, Currency::GBP)->get();
 print $rates['EUR'];
