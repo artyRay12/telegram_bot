2d69bd7ce59f2ec4cb5f8656b579f704ffddd8c8
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-30 03:40:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,11 @@ if ($text) {
     }
     $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
     $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-    $keyboard = array(
+  }
+}
+ 
+/*
+$keyboard = array(
     array(array('callback_data'=>'/butt1','text'=>'Кнопка 1')),
     array(array('callback_data'=>'/buut2','text'=>'Кнопка 2')),
     );
@@ -40,10 +44,8 @@ if ($text) {
         'text' => 'Нажмите на одну из кнопок:',
         'reply_markup' => $reply_markup,
     ));
-  }
-}
- 
-/*$rates = (new Exchange())->key("f22838f03ab3c8f3ff5f7e119f870dfe")->symbols(Currency::USD, Currency::GBP)->get();
+
+$rates = (new Exchange())->key("f22838f03ab3c8f3ff5f7e119f870dfe")->symbols(Currency::USD, Currency::GBP)->get();
 print $rates['EUR'];
 print $rates[Currency::GBP];*/
 ?>
