101472ea321ac7d630b09b2d86dda2fc0193c305
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-30 04:26:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if($text) {
         $reply = "Welcome";
         $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 
                                                         'resize_keyboard' => true, 
-                                                        'one_time_keyboard' => false ]);
+                                                        'one_time_keyboard' => true ]);
         $telegram->sendMessage([ 'chat_id' => $chat_id, 
                                 'text' => $reply, 
                                 'reply_markup' => $reply_markup ]);
