da66a2e638968d7de1531ad9fc9d53861533897f
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-06-30 18:10:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ use Telegram\Bot\Api;
 use Fadion\Fixerio\Exchange;
 use Fadion\Fixerio\Currency;
 
-/*$exchange = new Exchange();
+$exchange = new Exchange();
 $exchange->key('f22838f03ab3c8f3ff5f7e119f870dfe');
 $exchange->base(Currency::USD);
 $exchange->symbols(Currency::EUR, Currency::GBP);
@@ -16,33 +16,47 @@ $text = $result["message"]["text"]; //Текст сообщения
 $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
 $keyboard = [["Start"]]; //Клавиатура
+$start = FALSE;
 
-/*function sayHello($message) {
-    $message = "You pressed button";
-    return $message;
-{*/
-    
-if($text) {
+
+if ($text) {
     if ($text == "/start") {
         $reply = "Welcome";
-        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard,
+        $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard,
             'resize_keyboard' => true,
-            'one_time_keyboard' => false ]);
-    } 
-    elseif($text == "/sayHello") {
+            'one_time_keyboard' => true]);
+    } elseif ($text == "/sayHello") {
         if ($name) {
             $reply = 'Hello, ' . $name . '!';
         } else {
             $reply = 'Hello, stranger!';
         }
-    } 
-    elseif($text == "Start") {
-        $reply = "You Dumb";
-    }  
+    } elseif ($text == "Start") {
+        $reply = "Game is started!!!";
+        $start = TRUE;
+        $keyboard = [["Какая нахрен разница"]]; //Клавиатура
+    }
 }
-$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-/*
-$rates = (new Exchange())->key("f22838f03ab3c8f3ff5f7e119f870dfe")->symbols(Currency::USD, Currency::GBP)->get();
+
+if ($start) {
+    $reply = "Чем отличается светло-розовый цвет, от нересто-лососевого";
+    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
+    $keyboard = [["Какая нахрен разница"]]; //Клавиатура
+    $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard,
+        'resize_keyboard' => true,
+        'one_time_keyboard' => true]);
+    if ($text) {
+        if ($text == "Какая нахрен разница") {
+            $reply = "Правильно!";
+        }
+    }
+    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
+}
+
+
+
+/*$rates = (new Exchange())->key("f22838f03ab3c8f3ff5f7e119f870dfe")->symbols(Currency::USD, Currency::GBP)->get();
 print $rates['EUR'];
 print $rates[Currency::GBP];*/
+
 ?>
