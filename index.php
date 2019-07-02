f966dca1acefde3700a38af1e698dd638e833e43
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-07-02 19:45:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -32,11 +32,11 @@ $db = new MysqliDb ($heroku_host, $heroku_userName, $heroku_pass, $heroku_schema
 
 //if ($text) {
   if ($text == "/start") {
+    $keyboard = [["Начать игру"]];
+    $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => true]);
     $data = Array ('dynamicQuestID' => '0');
     $db->update ('questions', $data);
     $telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'Test was reloaded', 'reply_markup' => $reply_markup]);
-    $keyboard = [["Начать игру"]];
-    $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => true]);
   } else {
     
     //----===Берем questID
