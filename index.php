7bfd18b7dc37dd2cae279761db5d0105d903b95e
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-07-03 06:51:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $db = new MysqliDb ($heroku_host, $heroku_userName, $heroku_pass, $heroku_schema
 
 try {
   if ($text == "/start") {
-    $data = Array ('dynamicQuestID' => 0);
+    $data = Array ('dynamicQuestID' => 1);
     $db->update ('questions', $data);
     //$telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'Test was reloaded', 'reply_markup' => $reply_markup]);
   }
