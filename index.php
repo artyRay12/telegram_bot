01bb16e67a54a0a88f69a97e5af3e3d94c03529a
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-07-03 18:52:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -57,6 +57,12 @@ try {
   }
 
   if($questDinId <= 5) {  
+    //----===Получаем score
+    $scoreRequest = Array("userScore");
+    $scoreDb = $db->get("users", null, $scoreRequest);
+    $score = isset($scoreDb[0]["userScore"]) ? $scoreDb[0]["userScore"] : "";
+    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $score, 'reply_markup' => $reply_markup]);
+    
     //----===Берем questID
     $questIdRequest = Array("dynamicQuestID"); //Массив для с полем для запроса
     $questIdDb = $db->get ("questions", null, $questIdRequest);//получаем номер квеста
