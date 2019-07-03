e1a12ab7bf778699dbdff035ce4dd6aa650df6d0
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-07-04 01:35:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ function anwerAnalys($text, $questDinId, $score, $answer1, $answer2, $answer3, $
 function checkUserID($db, $userID, $name, $id) {
   $db -> where("userName", $name);
   $userData = $db->getOne("users");
-  if (empty($userData)) {
+  if ($userData) {
   } else {
     $data = Array ("userID" => $userID,
                "userName" => $name,
