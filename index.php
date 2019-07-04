176627df71e7167cb3db0007dfb938b7cd81c4cb
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-07-04 07:14:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -156,7 +156,7 @@ try {
       $questTextRequest = Array ("questText");
       $questDb = $db->get ("questions", null, $questTextRequest);
       $questText = $questText = isset($questDb[$questDinId]["questText"]) ? $questDb[$questDinId]["questText"] : "";  
-      $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $questText, reply_markup' => $reply_markup]);
+      $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $questText, 'reply_markup' => $reply_markup]);
     }
   
     //----===Увеличиваю счетчик вопроса
