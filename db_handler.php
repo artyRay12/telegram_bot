96c063d1c773212cd169e6efbd5c648866460788
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-07-07 03:49:47 +0300

    Update db_handler.php

--- db_handler.php
+++ db_handler.php
@@ -2,7 +2,7 @@
     require_once("config.php");
 
     function dbInit(): mysqliDB {
-        $db = new mysqlibDb(HEROKU_DB_HOST, HEROKU_DB_USER, HEROKU_DB_PASS, HEROKU_DB_NAME);
+        $db = new mysqliDb(HEROKU_DB_HOST, HEROKU_DB_USER, HEROKU_DB_PASS, HEROKU_DB_NAME);
         $db->autoReconnect = TRUE;
         return $db;
     }
