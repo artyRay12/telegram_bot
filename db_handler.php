77dd1c918e6b94a72c532893a8e98ea318bcabff
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-07-07 03:48:11 +0300

    Add files via upload

---
+++ db_handler.php
@@ -0,0 +1,10 @@
+<?php
+    require_once("config.php");
+
+    function dbInit(): mysqliDB {
+        $db = new mysqlibDb(HEROKU_DB_HOST, HEROKU_DB_USER, HEROKU_DB_PASS, HEROKU_DB_NAME);
+        $db->autoReconnect = TRUE;
+        return $db;
+    }
+
+?>
