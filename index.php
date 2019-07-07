6e48581b1041addd07acc81909e8d0a5a1523918
artyRay12 <artysha.rylit12@gmail.com>
2019-07-07 12:40:25 +0300

    add func

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 
     $telegram = new Api('713953239:AAFiRmir3z-JsMnDMmGdQ4twvV2nzLpADGs');
     $result = $telegram -> getWebhookUpdates();
-    $text = $result["message"]["text"]; //Текст сообщения
-    $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
-    $userName = $result["message"]["from"]["username"]; //Юзернейм пользователя
+    $text = $result["message"]["text"];
+    $chat_id = $result["message"]["chat"]["id"];
+    $userName = $result["message"]["from"]["username"];
     $userID = $result['message']['from']['id'];
     $db = dbInit();
 
@@ -29,18 +29,20 @@
         if (isRightAnswer($db, $userID, $update, $text)) {
             addPoint($db, $userID);
         }
-
         $keyboard = getPosibleAnswers($update, $userID, $db);
-        $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => true]);
+        $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard,
+                                                        'resize_keyboard' => true,
+                                                        'one_time_keyboard' => true]);
 
         increaseQuestCounter($db, $userID);
-
-        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => getQuestText($update), 'reply_markup' => $reply_markup]);
+        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => getQuestText($update),
+                                                       'reply_markup' => $reply_markup]);
 
     } else {
         $keyboard = [[START_COMMAND]];
-        $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => true]);
-
+        $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard,
+                                                        'resize_keyboard' => true,
+                                                        'one_time_keyboard' => true]);
         if (isRightAnswer($db, $userID, $update, $text)) {
           addPoint($db, $userID);
         }
@@ -49,7 +51,9 @@
             addPersonalRecord($db, $userID);
         }
 
-        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Вы набрали всего лишь: " . getUserScore($db, $userID) . " баллов", 'reply_markup' => $reply_markup]);
+        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Вы набрали всего лишь: " . getUserScore($db, $userID)
+                                                        . " баллов",
+                                                        'reply_markup' => $reply_markup]);
  }
 
 ?>
