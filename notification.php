907fc99ff3ac716a031f15917cabb50ce552dc1f
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-07-10 02:52:24 +0300

    Update notification.php

--- notification.php
+++ notification.php
@@ -1,9 +1,5 @@
 <?php
   $chatID = 843008832;
-  require('index.php');
-  $telegram->sendMessage(['chat_id' => $chatID, 'text' => "Привет",
-    'reply_markup' => $reply_markup]);
-
-
-
+  require("index.php");
+  $telegram->sendMessage(['chat_id' => $chatID, 'text' => "Привет"]);
 
