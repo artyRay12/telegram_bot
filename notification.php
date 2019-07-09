c9c6a10897948d04736dd551dd46ddb5bfad56dc
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-07-10 02:59:56 +0300

    Update notification.php

--- notification.php
+++ notification.php
@@ -1,6 +1,6 @@
 <?php
+  require("index.php");
   $chatID = 843008832;
   use Telegram\Bot\Api;
   $telegram = new Api('713953239:AAFiRmir3z-JsMnDMmGdQ4twvV2nzLpADGs');
-  require("index.php");
   $telegram->sendMessage(['chat_id' => $chatID, 'text' => "Привет"]);
