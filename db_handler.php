a1de0c9c182cc218a517ecc0baab5eb5e0729bef
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-07-10 06:06:40 +0300

    Update db_handler.php

--- db_handler.php
+++ db_handler.php
@@ -13,7 +13,34 @@
         $userInfo = $db->getOne("users", null, "userID");
         $userInfo["userID"] = isset($userInfo["userID"]) ? $userInfo["userID"] : "";
         return $userInfo["userID"];
+    }  
+    
+    function getInfoByID($info, $db, $userID) {
+    if ($info == USER_ID) {
+        $db->where('userID', $userID);
+        $userInfo = $db->getOne("users", null, "userID");
+        $userInfo["userID"] = isset($userInfo["userID"]) ? $userInfo["userID"] : "";
+        return $userInfo["userID"];
+    } elseif ($info == RIGHT_ANSWER) {
+        $db->where('userID', $userID);
+        $rightAnswer = $db->get("users", null, "rightAnswer");
+        return isset($rightAnswer[0]["rightAnswer"]) ? $rightAnswer[0]["rightAnswer"] : "";
+    } elseif ($info == CURRENT_QUEST_ID) {
+        $db->where('userID', $userID);
+        $questId = $db->get("users", null, "currentQuest");//получаем номер квеста
+        return isset($questId[0]["currentQuest"]) ? $questId[0]["currentQuest"] : "";
+    } elseif ($info == USER_SCORE) {
+        $db->where('userID', $userID);
+        $scoreDb = $db->get("users", null, "userScore");
+        return isset($scoreDb[0]["userScore"]) ? $scoreDb[0]["userScore"] : "";
+    } elseif ($info == USER_MAX_SCORE) {
+        $db->where('userID', $userID);
+        $scoreDb = $db->get("users", null, "maxScore");
+        return isset($scoreDb[0]["maxScore"]) ? $scoreDb[0]["maxScore"] : "";
     }
+}
+
+
 
     function createNewAccount($db, $userID, $userName): void {
         $query = "insert into users(userID, userName, userScore, currentQuest, maxScore, rightAnswer) values('$userID', '$userName', '0', '0', '0', '0')" or die(mysqli_error($db));
