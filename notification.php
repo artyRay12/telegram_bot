9053d26225a7c6d8a0ada60546765d75ab4e8868
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-07-10 03:46:52 +0300

    Update notification.php

--- notification.php
+++ notification.php
@@ -1,8 +1,9 @@
 <?php
-  //require("index.php");
-  include('vendor/autoload.php');
-  $chat_id = $result["message"]["chat"]["id"];
-  $chatID = 843008832;
   use Telegram\Bot\Api;
   $telegram = new Api('713953239:AAFiRmir3z-JsMnDMmGdQ4twvV2nzLpADGs');
-  $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Привет"]);
+
+  $usersID = $db->rawQuery('SELECT DISTINCT(userID) FROM ' . 'users');
+  $length = count($usersID);
+  for($i = 0; $i < $length; $i++) {
+      $telegram->sendMessage(['chat_id' => $usersID[$i]["userID"], 'text' => "Привет"]);
+  }
