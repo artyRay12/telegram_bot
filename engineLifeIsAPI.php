1b0abce256e5fd619c76aeecfdd67c783ad418ea
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2019-07-29 10:31:07 +0300

    Update engineLifeIsAPI.php

--- engineLifeIsAPI.php
+++ engineLifeIsAPI.php
@@ -4,12 +4,12 @@
   $questionsRequest = "https://engine.lifeis.porn/api/millionaire.php?q=$questLevels";
   $questionRequest = json_decode(file_get_contents($questionsRequest), JSON_OBJECT_AS_ARRAY);
 
-  function getPosibleAnswers($questionRequest, $userID, $db): array {
+  function getPosibleAnswers($questionRequest, $userID, $db): array { //получаем варианты ответов с API
        $answersID = [];
        $isAnswersReady = FALSE;
        $answersCounter = 0;
-       pushRightAnswerInDB($db, $userID, $questionRequest);
-       while ($isAnswersReady == FALSE):
+       pushRightAnswerInDB($db, $userID, $questionRequest);  //сохраняем правильный вариант ответа в БД, чтобы потом сравнить.
+       while ($isAnswersReady == FALSE):  //этот цикл замешивает числа от 0-3 рандомно в массив, чтобы варианты ответа выводились в разном порядке
            $randID = rand(0, 3);
            if (in_array($randID, $answersID)) {
            } else {
